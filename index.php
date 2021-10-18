b38053ae9b1c79b40e9e9f0dcb83415f55eb105c
steftes99 <stefanotescione83@gmail.com>
2021-10-18 10:23:37 +0200

    init

---
+++ index.php
@@ -0,0 +1,4 @@
+<!-- Esercizio di oggi: cartella/repo php-ajax-dischi Stampare a schermo una decina di dischi musicali (vedi screenshot). 
+Utilizzare: Html, Sass, JS, VueJS, PHP Prima Milestone: Stampiamo i dischi solo con l’utilizzo di PHP, che stampa direttamente i dischi in pagina: al caricamento della pagina ci saranno tutti i dischi. 
+Seconda Milestone: Attraverso l’utilizzo di axios: al caricamento della pagina axios chiederà, attraverso una chiamata api, i dischi a php e li stamperà attraverso vue. 
+Bonus: Attraverso un’altra chiamata api, filtrare gli album per genere. -->
